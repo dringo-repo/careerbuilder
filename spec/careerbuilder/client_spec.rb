2852d8e543a4a1872028f40b09ce1546703ea8ee
Frank Olson <will.olson11@gmail.com>
2017-04-22 08:18:39 -0700

    add specs titles to client

--- spec/careerbuilder/client_spec.rb
+++ spec/careerbuilder/client_spec.rb
@@ -1,7 +1,14 @@
 require_relative '../spec_helper'
 
 describe Careerbuilder::Client do
+  let(:url) { 'example' }
+  let(:params) { {test: 123} }
+  let(:api_token) do
+    "#{Careerbuilder.configuration.api_token}"
+  end
+  
   context '#get' do
+    it 'merges api_token with query params'
     it 'passes in query params'
   end
 end
